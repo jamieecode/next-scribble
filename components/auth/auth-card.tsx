2bb38fa488d7b328210e5b3a54ce4f395f835dd9
jamieecode <jamieecode@gmail.com>
2024-08-04 15:38:03 +0900

    style: style nav, form

---
+++ components/auth/auth-card.tsx
@@ -0,0 +1,43 @@
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Avatar } from "../ui/avatar";
+import Socials from "./socials";
+import { BackButton } from "./back-button";
+
+type CardWrapperProps = {
+  children: React.ReactNode;
+  cardTitle: string;
+  backButtonHref: string;
+  backButtonLabel: string;
+  showSocials?: boolean;
+};
+
+export const AuthCard = ({
+  children,
+  cardTitle,
+  backButtonHref,
+  backButtonLabel,
+  showSocials,
+}: CardWrapperProps) => {
+  return (
+    <Card>
+      <CardHeader>
+        <CardTitle>{cardTitle}</CardTitle>
+        <CardContent>{children}</CardContent>
+        {showSocials && (
+          <CardFooter>
+            <Socials />
+          </CardFooter>
+        )}
+        <CardFooter>
+          <BackButton href={backButtonHref} label={backButtonLabel} />
+        </CardFooter>
+      </CardHeader>
+    </Card>
+  );
+};
