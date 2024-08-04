b0124b427ce01a33dcae7e96136cb23772df5346
jamieecode <jamieecode@gmail.com>
2024-08-04 14:40:03 +0900

    feat: add navbar

--- server/auth.ts
+++ server/auth.ts
@@ -8,6 +8,7 @@ import { db } from "@/server";
 export const { handlers, auth, signIn, signOut } = NextAuth({
   adapter: DrizzleAdapter(db),
   secret: process.env.AUTH_SECRET,
+  session: { strategy: "jwt" },
   providers: [
     Google({
       clientId: process.env.GOOGLE_CLIENT_ID,
